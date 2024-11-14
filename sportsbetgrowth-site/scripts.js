eccf3ba9190374b67d1fb64a16da9a1d45f89a2b
sportsbetgrowth <sportsbetgrowth@gmail.com>
2024-11-14 11:31:10 +0000

    Hamburger Changes

--- sportsbetgrowth-site/scripts.js
+++ sportsbetgrowth-site/scripts.js
@@ -66,4 +66,11 @@ if (imageSectionButton) {
     });
 }
 
+// Hamburger Menu Toggle
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
 
