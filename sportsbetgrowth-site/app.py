d5200dd9388c1fc85e7d9c0dafe45a1188e79fb4
Gerard <sportsbetgrowth@gmail.com>
2024-12-31 18:01:59 +0200

    Updates to include working CMS

--- sportsbetgrowth-site/app.py
+++ sportsbetgrowth-site/app.py
@@ -1,24 +1,62 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 import json
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Enable Cross-Origin Resource Sharing
 
-with open('blogs.json') as f:
-    blogs = json.load(f)
+@app.route('/<path:filename>')
+def serve_static_files(filename):
+    return send_from_directory('.', filename)
 
+# Load blogs from JSON file
+def load_blogs():
+    with open('blogs.json', 'r') as f:
+        return json.load(f)
+
+# Save blogs to JSON file
+def save_blogs(blogs):
+    with open('blogs.json', 'w') as f:
+        json.dump(blogs, f, indent=4)
+
+# Serve Frontend Pages
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/blog')
+def blog():
+    return send_from_directory('.', 'blog.html')
+
+@app.route('/blog-detail')
+def blog_detail():
+    return send_from_directory('.', 'blog-detail.html')
+
+# CMS Admin Page
+@app.route('/admin', methods=['GET'])
+def admin():
+    return send_from_directory('.', 'admin.html')
+
+# API Endpoints
 @app.route('/blogs', methods=['GET'])
 def get_blogs():
-    return jsonify(blogs)
+    return jsonify(load_blogs())
 
-@app.route('/blogs/<int:id>', methods=['GET'])
-def get_blog(id):
-    blog = next((b for b in blogs if b["id"] == id), None)
-    if blog:
-        return jsonify(blog)
-    return {"error": "Blog not found"}, 404
+@app.route('/blogs', methods=['POST'])
+def add_blog():
+    blogs = load_blogs()
+    new_blog = request.json
+    new_blog['id'] = max([b['id'] for b in blogs], default=0) + 1
+    blogs.append(new_blog)
+    save_blogs(blogs)
+    return jsonify(new_blog), 201
+
+@app.route('/blogs/<int:id>', methods=['DELETE'])
+def delete_blog(id):
+    blogs = load_blogs()
+    blogs = [blog for blog in blogs if blog['id'] != id]
+    save_blogs(blogs)
+    return {"message": "Blog deleted"}, 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
-    
