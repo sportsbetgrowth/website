0d9b3d9343b32dbb3cabb39c71ef1e481b49a68b
Gerard <sportsbetgrowth@gmail.com>
2025-01-01 12:24:53 +0200

    Update to app.py

--- sportsbetgrowth-site/app.py
+++ sportsbetgrowth-site/app.py
@@ -38,4 +38,4 @@ def header_footer(filename):
     return send_from_directory('header-footer', filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
